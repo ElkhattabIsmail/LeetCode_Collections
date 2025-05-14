ed85dd0ea51f8c38aa0bfbcd90f685892c78ca3c
ELKHATTAB SMAIL <elhattab207@gmail.com>
2025-05-14 14:11:01 +0100

    Product Sales Analysis using CTE.

---
+++ SQL/02- Medium/06- Product Sales Analysis III.sql
@@ -0,0 +1,93 @@
+
+
+-- Question:
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+
+Write a solution to select the product id, year, quantity, and price for the first year of every product sold. If any product is bought multiple times in its first year, return all sales separately.
+
+Return the resulting table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+
+
+-- Answer:
+
+
+-- 01
+select S.product_id, FirstSale.first_year,
+S.quantity, S.price 
+from Sales S right Join 
+(
+SELECT product_id, MIN(year) AS first_year
+ FROM Sales GROUP BY product_id       
+) as FirstSale       
+ON FirstSale.product_id = S.product_id 
+AND FirstSale.first_year = S.year
+ORDER BY FirstSale.product_id;
+
+
+-- 02 Using Common Table Expression :
+WITH FirstYear AS (
+  SELECT product_id, MIN(year) AS first_year
+  FROM Sales
+  GROUP BY product_id
+)
+SELECT S.product_id, F.first_year, S.quantity, S.price
+FROM Sales S
+JOIN FirstYear F
+ON S.product_id = F.product_id AND S.year = F.first_year;
+    
+
