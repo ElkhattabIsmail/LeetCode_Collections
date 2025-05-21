f45f9bcf495ea46067a57c5e68c79a33bf65ac0d
ELKHATTAB SMAIL <elhattab207@gmail.com>
2025-05-21 15:39:32 +0100

    Last Person to Fit in the Bus using Joining Approach.

--- SQL/02- Medium/10- Last Person to Fit in the Bus.sql
+++ SQL/02- Medium/10- Last Person to Fit in the Bus.sql
@@ -75,4 +75,12 @@ where t1.Total_weight <= 1000
 order by t1.turn desc
 
 
+-- using Joining Approach.
+
+SELECT TOP 1 q1.person_name
+FROM Queue q1
+JOIN Queue q2 ON q2.turn <= q1.turn
+GROUP BY q1.person_id, q1.person_name, q1.turn
+HAVING SUM(q2.weight) <= 1000
+ORDER BY q1.turn DESC;
 
